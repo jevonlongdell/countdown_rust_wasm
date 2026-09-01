ac4f16ca935c5cdf15f36afd49af5f6ff4a47846
Jevon Longdell (f2) <jevon.longdell@gmail.com>
2026-09-01 20:11:28 +1200

    now just need logic that solves the puzzle

--- src/app.rs
+++ src/app.rs
@@ -5,16 +5,16 @@ pub struct TemplateApp {
     // Example stuff:
     label: String,
 
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+//    #[serde(skip)] // This how you opt-out of serialization of a field
+//    value: f32,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
             // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            label: "asdfjkle".to_owned(),
+//            value: 2.7,
         }
     }
 }
@@ -61,35 +61,38 @@ impl eframe::App for TemplateApp {
                     ui.add_space(16.0);
                 }
 
-                egui::widgets::global_theme_preference_buttons(ui);
+                //egui::widgets::global_theme_preference_buttons(ui);
             });
         });
 
         egui::CentralPanel::default().show(ui, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+            ui.heading("Countdown word game");
 
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
+                ui.label("Given letters: ");
                 ui.text_edit_singleline(&mut self.label);
             });
 
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
 
             ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
+	    for k in (3..=9).rev() {
+		ui.heading(format!("{k} letter words:"));
+		ui.label("pumpkin, turnip, sweede");
+		ui.label("");
+	    }
+	    
+         
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+        	egui::widgets::global_theme_preference_buttons(ui);
                 powered_by_egui_and_eframe(ui);
                 egui::warn_if_debug_build(ui);
-            });
+		   ui.add(egui::github_link_file!(
+                "https://github.com/jevonlongdell/countdown_rust_wasm",
+                "Source code."
+            ));
+	    });
         });
     }
 }

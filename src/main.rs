b0d820aa1b63934e0d9cc2b8d43d98bb08be7105
Jevon Longdell <jevon.longdell@gmail.com>
2026-09-02 17:22:27 +1200

    finished :)

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "Countdown",
         native_options,
         Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
     )
